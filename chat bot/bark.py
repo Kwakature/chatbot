18a14fd2620c46265189748ec36fdd6d26b86404
kwakature <berton.arthur24@gmail.com>
2024-11-15 22:43:03 +0100

    ajoue de bark

--- chat bot/bark.py
+++ chat bot/bark.py
@@ -1,16 +1,17 @@
 from transformers import AutoProcessor, BarkModel
 import scipy
 
-processor = AutoProcessor.from_pretrained("suno/bark")
-model = BarkModel.from_pretrained("suno/bark")
+def bark_primpt(phrase):
+    processor = AutoProcessor.from_pretrained("suno/bark")
+    model = BarkModel.from_pretrained("suno/bark")
 
-voice_preset = "v2/en_speaker_1"
+    voice_preset = "v2/en_speaker_1"
 
-inputs = processor("Hello, my dog is cute", voice_preset=voice_preset)
+    inputs = processor(str(phrase), voice_preset=voice_preset)
 
-audio_array = model.generate(**inputs)
-audio_array = audio_array.cpu().numpy().squeeze()
+    audio_array = model.generate(**inputs)
+    audio_array = audio_array.cpu().numpy().squeeze()
 
-sample_rate = model.generation_config.sample_rate
-scipy.io.wavfile.write("bark_out.wav", rate=sample_rate, data=audio_array)
+    sample_rate = model.generation_config.sample_rate
+    scipy.io.wavfile.write("bark_out.wav", rate=sample_rate, data=audio_array)
 
